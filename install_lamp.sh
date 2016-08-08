bd4bfd3b9f7ce405f6cc1844f01856eb16891efd
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-08-08 15:43:37 +0000

    Adding PHP 7, Git and Composer to AWS LAMP installation.

--- install_lamp.sh
+++ install_lamp.sh
@@ -2,7 +2,7 @@
 # My first script
 
 sudo yum update -y
-sudo yum install -y httpd24 php56 mysql55-server php56-mysqlnd
+sudo yum install -y httpd24 php70 mysql55-server php70-mysqlnd php70-mbstring git-all
 sudo service httpd start
 sudo chkconfig httpd on
 chkconfig --list httpd
@@ -16,3 +16,9 @@ sudo service mysqld start
 sudo mysql_secure_installation
 sudo service mysqld stop
 sudo chkconfig mysqld on
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
