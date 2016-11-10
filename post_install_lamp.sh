9bd91608398d0df75edc129182d6e1e7f4527320
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-11-10 22:04:10 +0000

    idem

---
+++ post_install_lamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# My first script
+
+sudo chown -R ec2-user:www /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
