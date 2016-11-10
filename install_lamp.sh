c8ac9c02c60a1cef1736131e7291f295b1a557a1
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-11-10 21:59:26 +0000

    Fix install commands for modx installation in AWS.

--- install_lamp.sh
+++ install_lamp.sh
@@ -2,23 +2,18 @@
 # My first script
 
 sudo yum update -y
-sudo yum install -y httpd24 php70 mysql55-server php70-mysqlnd php70-mbstring git-all
+sudo yum install -y httpd24 php56 mysql55-server php56-mysqlnd php56-mbstring git-all
 sudo service httpd start
 sudo chkconfig httpd on
 chkconfig --list httpd
 sudo groupadd www
 sudo usermod -a -G www ec2-user
-sudo chown -R root:www /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
 sudo service mysqld start
 sudo mysql_secure_installation
 sudo service mysqld stop
 sudo chkconfig mysqld on
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
+sudo mv composer.phar /usr/local/bin/composer
